fe41745c9a6f2618a4ea6bcb584f271b293331cf
khensura.loveel Love EL <khensura.loveel@gmail.com>
2018-11-07 22:26:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-function add(a, b)
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return b - a;
+}
+
+function multiply(a, b) {
+  
+  return a * b;
+}
+
+
+function divide(a, b) {
+  return a/b ;
+}
+
+function inc(c) {
+   c++;
+   return c;
+}
+
+function dec(c) {
+  c--;
+  return c;
+}
+
+function makeInt(n) {
+  return parseInt(n, 10);
+  
+}
+
+function preserveDecimal (n) {
+  return parseFloat(n);
+}
